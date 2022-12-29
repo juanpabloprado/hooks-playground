47b07f8c3af3111fc67f57ae15203d7b8ab66d68
Juan Prado <juan@juanpabloprado.com>
2022-12-29 11:03:02 -0600

    add toggle button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [activated, setActivated] = useState(false)
+    const buttonText = activated ? "Active" : "Inactive";
+
+    const onClick = () => setActivated(!activated);
+
+    return (
+        <button onClick={onClick}>{buttonText}</button>
+    );
 }
 
 export default App;
