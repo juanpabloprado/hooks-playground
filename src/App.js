0926f752370531948f0e71b14cb17ede343b4ceb
Juan Prado <juan@juanpabloprado.com>
2022-12-29 11:20:00 -0600

    add decrement and reset to example

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import {useState} from "react";
 function App() {
     const [count, setCount] = useState(0)
 
-    const handleClick = () => setCount(count + 1);
-    // const handleClick = () => setCount(prevCount => prevCount + 1)
+    const incrementClick = () => setCount(count + 1);
+    // const incrementClick = () => setCount(prevCount => prevCount + 1)
+    const decrementClick = () => setCount(count - 1);
+    // const decrementClick = () => setCount(prevCount => prevCount - 1)
+    const resetClick = () => setCount(0);
 
     return (
        <div>
-           <button onClick={handleClick}>Increment</button>
+           <button onClick={incrementClick}>Increment</button>
+           <button onClick={decrementClick}>Decrement</button>
+           <button onClick={resetClick}>Reset</button>
            <h1>{count}</h1>
        </div>
     );
