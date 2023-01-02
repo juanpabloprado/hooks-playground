7a098fd9c24bd1cac268e95a45c5e712fc7b6e93
Juan Prado <juan@juanpabloprado.com>
2023-01-02 16:29:16 -0600

    track count

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import {useEffect, useState} from "react";
 
 function Counter() {
     const [count, setCount] = useState(0);
+    const [color, setColor] = useState("salmon");
     const handleIncrement = () => setCount(count + 1);
     const handleDecrement = () => setCount(count - 1);
+
+    const handleColorChange = () => {
+        const nextColor = color === "salmon"? "gold" : "salmon";
+        setColor(nextColor);
+    }
 
     useEffect(() => {
         console.log(`I'm inside the useEffect function. The current count is ${count}`);
@@ -11,12 +17,13 @@ function Counter() {
         return () => {
             console.log(`I'm removing anything that was set up above! The last count was ${count}`);
         }
-    });
+    }, [count]);
     return (
         <div>
             <button onClick={handleIncrement}>Increment</button>
+            <button onClick={handleColorChange}>Change the color</button>
             <button onClick={handleDecrement}>Decrement</button>
-            <h1>Count: {count}</h1>
+            <h1 style={{color}}>Count: {count}</h1>
         </div>
     );
 }
