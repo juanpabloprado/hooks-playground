416073b446062d118eaa0179e386faa548b2dc57
Juan Prado <juan@juanpabloprado.com>
2023-01-02 17:29:20 -0600

    react context

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import {useEffect, useState} from "react";
-function App() {
-    const [userText, setUserText] = useState('');
+import React, {Component, createContext} from "react";
 
-    const handleUserKeyPress = (event) => {
-        const { key, keyCode } = event
-        if (keyCode === 32 || (keyCode >= 65 && keyCode <= 90)) {
-            setUserText(`${userText}${key}`)
-        }
-    };
-    useEffect(() => {
-        window.addEventListener('keydown', handleUserKeyPress)
-        return () => {
-            window.removeEventListener('keydown', handleUserKeyPress)
-        }
-    });
+const NameContext = createContext("");
 
-    return (
-        <div>
-            <h1>Feel free to type! Your text will show up below</h1>
-            <blockquote>
-                { userText }
-            </blockquote>
-        </div>
-    )
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "Juan Pablo Prado"
+        }
+    }
+
+    render() {
+        return (
+            <NameContext.Provider value={this.state.name}>
+                <Child/>
+            </NameContext.Provider>
+        )
+    }
+}
+class Button extends Component {
+    render() {
+        return (
+            <NameContext.Consumer>
+                {name => <button>{name}</button>}
+            </NameContext.Consumer>
+        );
+    }
+}
+
+class GrandChild extends Component {
+    render() {
+        return (
+            <div className="grandchild">
+                <Button/>
+            </div>
+        )
+    }
+}
+
+class Child extends Component {
+    render() {
+        return (
+            <section className="child">
+                <GrandChild/>
+            </section>
+        )
+    }
 }
 
 export default App;
