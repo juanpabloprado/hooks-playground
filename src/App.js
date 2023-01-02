fb45bad79f3e7c7e4eb8cce0ff848880fde0cdb2
Juan Prado <juan@juanpabloprado.com>
2023-01-02 16:53:41 -0600

    setup and teardown keydown event listener

--- src/App.js
+++ src/App.js
@@ -1,41 +1,26 @@
 import {useEffect, useState} from "react";
-
-function Counter() {
-    const [count, setCount] = useState(0);
-    const [color, setColor] = useState("salmon");
-    const handleIncrement = () => setCount(count + 1);
-    const handleDecrement = () => setCount(count - 1);
-
-    const handleColorChange = () => {
-        const nextColor = color === "salmon"? "gold" : "salmon";
-        setColor(nextColor);
-    }
-
-    useEffect(() => {
-        console.log(`I'm inside the useEffect function. The current count is ${count}`);
-
-        return () => {
-            console.log(`I'm removing anything that was set up above! The last count was ${count}`);
-        }
-    }, [count]);
-    return (
-        <div>
-            <button onClick={handleIncrement}>Increment</button>
-            <button onClick={handleColorChange}>Change the color</button>
-            <button onClick={handleDecrement}>Decrement</button>
-            <h1 style={{color}}>Count: {count}</h1>
-        </div>
-    );
-}
 function App() {
-    const [visible, setVisible] = useState(false);
+    const [userText, setUserText] = useState('');
+
+    const handleUserKeyPress = (event) => {
+        const { key, keyCode } = event
+        if (keyCode === 32 || (keyCode >= 65 && keyCode <= 90)) {
+            setUserText(`${userText}${key}`)
+        }
+    };
+    useEffect(() => {
+        window.addEventListener('keydown', handleUserKeyPress)
+        return () => {
+            window.removeEventListener('keydown', handleUserKeyPress)
+        }
+    });
 
     return (
         <div>
-            <button onClick={() => setVisible(!visible)}>
-                Show / Hide the Counter component
-            </button>
-            {visible && <Counter/>}
+            <h1>Feel free to type! Your text will show up below</h1>
+            <blockquote>
+                { userText }
+            </blockquote>
         </div>
     )
 }
