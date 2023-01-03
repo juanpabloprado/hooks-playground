99170bb2a478fb8203272ded249b3485da586720
Juan Prado <juan@juanpabloprado.com>
2023-01-02 21:30:29 -0600

    reusing custom hooks

--- src/App.js
+++ src/App.js
@@ -1,46 +1,23 @@
 import React, {useState} from "react";
 
-function useCounter(startingCount = 0) {
-    const [count, setCount] = useState(startingCount);
+function useInput() {
+    const [value, setValue] = useState('');
 
     return {
-        count,
-        increment: () => setCount(count + 1),
-        decrement: () => setCount(count - 1)
+        value,
+        onChange: (e) => {
+            setValue(e.target.value);
+        },
     };
 }
-
-function Display(props) {
-    const {count, increment, decrement} = useCounter(props.start);
-
-    return (
-        <div>
-            <button onClick={increment}>Increment</button>
-            <button onClick={decrement}>Decrement</button>
-            <h1>Count: {count}</h1>
-        </div>
-    )
-}
-
-function FancyDisplay(props) {
-    const {count, increment, decrement} = useCounter(props.start);
-
-    return (
-        <section>
-            <button onClick={increment}>Increment</button>
-            <button onClick={decrement}>Decrement</button>
-            <h2>Count: {count}</h2>
-        </section>
-    )
-}
-
 function App() {
+
     return (
-        <>
-            <Display start={10}/>
-            <Display start={20}/>
-            <FancyDisplay start={30}/>
-        </>
+        <form>
+            <input type="text"placeholder="Name" { ...useInput() } />
+            <input type="text" placeholder="Surname" { ...useInput() } />
+            <input type="number" placeholder="Age" { ...useInput() } />
+        </form>
     )
 }
 
